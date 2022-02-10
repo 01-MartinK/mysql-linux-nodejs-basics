bd765ed6e53f9ac813373343bfa5b63343def3d5
Martin K <thedeadgamer101@gmail.com>
2022-02-10 15:21:00 +0200

    Creating database via code

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ let mysql = require('mysql');
 let connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '778Iv23N',
-    database : 'TEST',
+    password : '778Iv23N'
 });
 
 connection.connect(function(err) {
     if (err) throw err;
   
-    console.log('Connected to the MySQL server.');
+    console.log('Connected!');
+
+    connection.query("CREATE DATABASE oop_test", (err, result) => {
+        if (err) throw err;
+        console.log("Database created");
+    });
   });
