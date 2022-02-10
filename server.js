deee0d6bc4c7f39fd26ef376f9d422e082785822
Martin K <thedeadgamer101@gmail.com>
2022-02-10 15:23:26 +0200

    Table created

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ let mysql = require('mysql');
 let connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '778Iv23N'
+    password : '778Iv23N',
+    database: "oop_test"
 });
 
 connection.connect(function(err) {
@@ -11,8 +12,9 @@ connection.connect(function(err) {
   
     console.log('Connected!');
 
-    connection.query("CREATE DATABASE oop_test", (err, result) => {
+    var sql = "CREATE TABLE teachers (id int, name VARCHAR(32), address VARCHAR(120))";
+    connection.query(sql, (err, result) => {
         if (err) throw err;
-        console.log("Database created");
+        console.log("Table created");
     });
   });
