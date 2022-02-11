0d4ae0e3c267817adedec5f8028401f3a2289d10
MK <thedeadgamer101@gmail.com>
2022-02-11 19:02:37 +0200

    Displayed author article list

--- server.js
+++ server.js
@@ -59,6 +59,27 @@ app.get('/article/:slug', (req, res) => {
     });
 });
 
+app.get('/author/:id', (req, res) => {
+    let query = `SELECT * FROM author WHERE id=${req.params.id}`;
+    let author;
+    let articles = [];
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        author = result;
+        console.log(author)
+        query = `SELECT * FROM article WHERE author_id=${req.params.id}`
+        con.query(query, (err, result) => {
+            if (err) throw err;
+            article = result;
+             console.log(article)
+            res.render('author', {
+                author: author,
+                articles: article
+            });
+        });
+    });
+});
+
 app.listen(3000, () => {
     console.log('App started at localhost:3000')
 });
